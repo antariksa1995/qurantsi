8405cc6ad6bd9212c96b059985d9ee1e6fb5b4c4
byma4n <byma4n@gmail.com>
2024-08-21 05:17:38 +0700

    ee

--- src/components/dls/Footer/TitleAndDescription.tsx
+++ src/components/dls/Footer/TitleAndDescription.tsx
@@ -4,10 +4,15 @@ import useTranslation from 'next-translate/useTranslation';
 import styles from './Footer.module.scss';
 
 import Link, { LinkVariant } from '@/dls/Link/Link';
-import QuranTextLogo from '@/icons/logo-tsirwah.svg';
+import QuranTextLogo from '@/icons/quran-text-logo.svg';
 import { logButtonClick } from '@/utils/eventLogger';
 
 const TitleAndDescription = () => {
+  const { t } = useTranslation('common');
+
+  const onHiringAnnouncementClicked = () => {
+    logButtonClick('footer_hiring_announcement');
+  };
 
   return (
     <div className={styles.titleAndDescriptionContainer}>
