e7f2d48494f9aa90c19b497fc4a3ad9d564c03a8
byma4n <byma4n@gmail.com>
2024-08-21 05:44:14 +0700

    test

--- src/components/dls/Footer/TitleAndDescription.tsx
+++ src/components/dls/Footer/TitleAndDescription.tsx
@@ -4,7 +4,7 @@ import useTranslation from 'next-translate/useTranslation';
 import styles from './Footer.module.scss';
 
 import Link, { LinkVariant } from '@/dls/Link/Link';
-import QuranTextLogo from '@/icons/logo-tsirwah.svg';
+import QuranTextLogo from '@/icons/quran-text-logo.svg';
 import { logButtonClick } from '@/utils/eventLogger';
 
 const TitleAndDescription = () => {
@@ -20,6 +20,23 @@ const TitleAndDescription = () => {
         <div className={styles.iconContainer}>
           <QuranTextLogo />
         </div>
+        <div className={styles.title}>{t}</div>
+      </div>
+      <p className={styles.description}>{t()}</p>
+      <div className={styles.hiringAnnouncementContainer}>
+        <Trans
+          components={{
+            link: (
+              <Link
+                href="https://www.quran.foundation/careers"
+                variant={LinkVariant.Blend}
+                onClick={onHiringAnnouncementClicked}
+                isNewTab
+              />
+            ),
+          }}
+          i18nKey="common:footer.hiring"
+        />
       </div>
     </div>
   );
