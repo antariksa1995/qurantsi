7d0811a3400fbb612df409ff6f5144085a62a821
antariksa1995 <antariksa36@gmail.com>
2025-07-08 20:49:59 +0700

    Update TitleAndDescription.tsx

--- src/components/dls/Footer/TitleAndDescription.tsx
+++ src/components/dls/Footer/TitleAndDescription.tsx
@@ -1,3 +1,5 @@
+import Trans from 'next-translate/Trans';
+import useTranslation from 'next-translate/useTranslation';
 import styles from './Footer.module.scss';
 
 import Link, { LinkVariant } from '@/dls/Link/Link';
@@ -11,13 +13,23 @@ const TitleAndDescription = () => {
     logButtonClick('footer_hiring_announcement');
   };
 
-  return (
+ return (
     <div className={styles.titleAndDescriptionContainer}>
       <div className={styles.headingContainer}>
         <div className={styles.iconContainer}>
           <QuranTextLogo />
         </div>
+        <div className={styles.title}>{t('footer.title')}</div>
       </div>
+      <p className={styles.description}>
+        <Trans
+          i18nKey="common:footer.description"
+          components={{
+            br: <br />,
+            link: <Link href="https://quran.foundation" variant={LinkVariant.Blend} isNewTab />,
+          }}
+        />
+      </p>
     </div>
   );
 };
